4c0028ecf341dfd554a6b9514c7b4a0b10f966c7
Abhishek kumar singh <abhisheksinghbhtr967@gmail.com>
2024-10-18 14:10:20 +0530

    changin in notfound page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,7 +2,39 @@
 // import React from "react";
 
 const NotFound = () => {
-  return <h1>404 - Page Not Found</h1>;
+  return (
+    <h1>
+      404 - Page Not Found Lorem ipsum dolor sit, amet consectetur adipisicing
+      elit. A quas soluta mollitia deleniti voluptatum officiis sapiente eveniet
+      quibusdam dolorem suscipit omnis tenetur, dolor sequi quisquam tempora
+      expedita deserunt ducimus. Possimus saepe at necessitatibus adipisci
+      provident quasi autem omnis placeat excepturi, vel iusto, pariatur
+      deserunt temporibus! Molestias maxime laudantium laborum cupiditate
+      similique nostrum tempora? Dolores maiores rerum corporis, nisi minima
+      consectetur assumenda, repellat quae dignissimos id cumque quaerat! Nam
+      facere aliquid beatae excepturi necessitatibus officia nisi dolore odio?
+      At ipsa unde nobis voluptatum ad excepturi aspernatur eos, odio itaque
+      eveniet corporis aliquid et accusantium earum nesciunt perspiciatis dicta
+      eius necessitatibus ullam, assumenda molestiae? Pariatur, ex cum minima
+      repudiandae quam corrupti voluptatibus corporis, possimus quasi reiciendis
+      magnam. Libero nisi eligendi, nobis id illum facilis odit ea dolorum
+      magnam itaque beatae sint? Ducimus nemo reiciendis suscipit veritatis
+      deleniti tempore! Unde voluptate voluptates odit maiores ratione explicabo
+      voluptatem in non maxime? Error voluptatem dolorem consequuntur reiciendis
+      sit corrupti aliquam repellat ullam facilis ipsum! Dolorum repudiandae
+      corporis beatae error, delectus voluptatem soluta, sunt, hic illum dolores
+      necessitatibus aut vel consequuntur cupiditate totam nulla perspiciatis
+      quam possimus esse qui pariatur? Tenetur perferendis esse odit ex facere
+      ad culpa, quas, magni aperiam reprehenderit hic velit distinctio
+      obcaecati! Veritatis exercitationem aliquid consequuntur commodi amet quia
+      libero dolores maiores praesentium fuga iusto obcaecati perspiciatis a ea,
+      expedita quas pariatur tempore totam officia voluptate magni. Esse
+      asperiores ipsam sit molestias numquam explicabo, nostrum laborum
+      distinctio, delectus laboriosam aliquam cupiditate corrupti nihil
+      praesentium fugiat quia molestiae illum ea. Hic iure id veniam, optio
+      harum enim iusto!
+    </h1>
+  );
 };
 
 export default NotFound;
